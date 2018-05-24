bb87319fb22a11831c90c526e0b05cc3831485d2
Christopher Magruder <heymagruder@gmail.com>
2018-05-24 15:46:52 -0600

    animate updates on scroll
    js changes to make animate on scroll.

--- js/animate.js
+++ js/animate.js
@@ -4,21 +4,32 @@ const scrollTarget = document.querySelector('.scrollTarget');
 
 window.addEventListener('scroll', function(event) {
   // Y coordinate (in pixels) where our scrollTarget sits in the page
-  const scrollTargetTop = scrollTarget.offsetTop
+  const scrollTargetRect = scrollTarget.getBoundingClientRect()
+  const scrollTargetTop = scrollTargetRect.top
   
   // Offset (in pixels) above the scrollTarget element
-  const offsetAbove = 100
-  
-  // See where we're scrolling in the browser console (DevTools)
-  console.log(window.pageYOffset, scrollTargetTop)
-  
+  const offset = 200
+
+  const distance = window.innerHeight - scrollTargetTop
+  console.log(distance - offset)
+    
+  if (distance <= 0) {
+    thingToAnimate.classList.add('hidden')    
+  }
+
   // If we haven't scroll past our element / offset, remove the animation and stop the event handler...
-  if (window.pageYOffset <= scrollTargetTop - offsetAbove) {
+  if (distance - offset <= 0) {
     // Remove the animation
     thingToAnimate.classList.remove('animated', 'fadeInUp')
     return
   }
+
+
+  if (thingToAnimate.classList.contains('fadeInUp')) {
+    return
+  }
   
   // If we HAVE scrolled ON or PAST our element / offset, trigger the animation...
-  thingToAnimate.classList.add('animated', 'fadeInUp')  
+  thingToAnimate.classList.remove('hidden')
+  thingToAnimate.classList.add('animated', 'fadeInUp')
 })
