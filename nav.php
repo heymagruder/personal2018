245d7caad1264dcceacc0ef36010edcb410aa7b4
Christopher Magruder <heymagruder@gmail.com>
2018-05-31 16:13:05 -0600

    nav file to php test

---
+++ nav.php
@@ -0,0 +1,12 @@
+<? php
+echo '<nav>';
+echo '<ul>';
+echo '<a href='index.html' class="brand-logo">Logo</a>';
+echo '<div class="u-pull-right">';
+echo '<li class="brand-nav"><a href='index.html'>Work</a></li>';
+echo '<li class="brand-nav"><a href='about.html'>About</a></li>';
+echo '</div>';
+echo '</ul>';
+echo '</nav>';
+echo '';
+?>
