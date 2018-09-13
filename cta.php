9994b2c3fbbd249768346d8ceaec5db44afa406a
heymagruder <heymagruder@gmail.com>
2018-09-13 10:21:38 -0600

    fixing contact form

--- cta.php
+++ cta.php
@@ -1,5 +1,5 @@
 <div class="contact-cta u-full-width">
-  <p>Let's Make Something Awesome Today.</p>
+  <p class="cta-info">Let's Make Something Awesome Today.</p>
 	<button class="accordion">Say Hello</button>
 		<div class="panel">
 			  <section id="contact">
