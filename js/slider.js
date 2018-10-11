4308912925913e2c9f9d13c262bd1a9ec48ca320
heymagruder <heymagruder@gmail.com>
2018-10-10 21:29:35 -0600

    js edits.
    fixes

--- js/slider.js
+++ js/slider.js
@@ -12,7 +12,7 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  if (!slides) return
+  if (!slides || !slides.length) return
   var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1} 
     if (n < 1) {slideIndex = slides.length}
