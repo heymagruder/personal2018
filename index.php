32607417b7dbea7ab132c376935c0dfddbeada91
heymagruder <heymagruder@gmail.com>
2020-06-16 10:03:44 -0600

    revert

--- index.php
+++ index.php
@@ -78,16 +78,70 @@
 
   <!-- Primary Page Layout
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <div
+    class="container animsition"
+    data-animsition-in-class="fade-in-down-sm"
+    data-animsition-in-duration="1000"
+    data-animsition-out-class="fade-out-down-sm"
+    data-animsition-out-duration="800"
+  >
+ <?php include('nav.php'); ?>
 
-<!-- Primary Page Layout
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-
-    <div class="row" style="margin-top: 15%">
-      <div class="five columns about-me">
-        <h3>A little about me</h3>
-        <p>I've spent over a decade using design to help. Whether it's helping users navigate an experience or helping startups establish their brand. I've combined my passion for design and technology to create simple, engaging experiences for brands of all sizes.</p>
+    <div class="row">
+      <div class="ten columns" style="margin-top: 6%">
+        <p class="main-name">Christopher Magruder | Designer</p>
+        <h3 class="intro-text" id="shuffle">Design for Business, <br>Big and Small.</h3>
+        <p class="intro-para">Hello. I'm Christopher Magruder and I'm a designer.</p>
+        <p class="intro-para">I've spent the last fourteen years helping <span class="darker">build brands</span> of all sizes.</p>
       </div>
-</div><!-- End Container -->
+    </div>
+
+    <div class="row">
+      <div class="big-biz one-half column" style="margin-top: 8%">
+        <h6 class="biz">Big Business</h6>
+            <ol>
+                <li><a href="ihsmarkit">IHS Markit
+                  <p class="descriptor">Helping two business merge into one.</p>
+                </a></li>
+                <li><a href="morgan-stanley">Morgan Stanley
+                <p class="descriptor">Refining the users experience.</p>
+                </a></li>
+                <li><a href="usaa">USAA
+                <p class="descriptor">Redefining the Markets &amp; Research experience.</p>
+                </a></li>
+                <li><a href="pimco">PIMCO
+                <p class="descriptor">Global landing page design.</p>
+                </a></li>
+                <li><a href="bauer">Bauer Hockey
+                <p class="descriptor">A passion project</p>
+                </a></li>
+              </ol>
+      </div>
+
+      <div class="big-biz one-half column" style="margin-top: 8%">
+        <h6 class="biz">Small Business</h6>
+              <ol>
+                <li><a href="rapt-media">Rapt Media
+                <p class="descriptor">Helping build an interactive video platform.</p>
+                </a></li>
+                <li><a href="atour-foods">Atour Foods
+                <p class="descriptor">Branding a business from a kitchen to a dairy.</p>
+                </a></li>
+                <li><a href="colorado14ers">Colorado 14ers
+                <p class="descriptor">Redesigning Colorado's best hiking resource.</p>
+                </a></li>
+                <li><a href="keyzio">Keyzio
+                <p class="descriptor">Improving the user experience.</p>
+                </a></li>
+                <li><a href="logo-archive">Logo Archive
+                <p class="descriptor">A collection of logos, icons & brand elements.</p>
+                </a></li>
+              </ol>
+      </div>
+ </div><!-- End Container -->
+ <?php include('footer.php'); ?>
+ <?php include('footer-code.php'); ?>
+
 
 <!-- End Document
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->
